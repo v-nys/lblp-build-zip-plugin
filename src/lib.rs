62f22cf54625a602cbfff249a5c237df38fd1ded
Vincent Nys <vincentnys@gmail.com>
2025-08-07 09:07:56 +0200

    Can test this.

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,8 @@
+use base64::{engine::general_purpose::STANDARD as BASE64_ENGINE, Engine};
 use extism_pdk::*;
 use logic_based_learning_paths::domain_without_loading::{
-    ArchivePayload, EdgeType, ParamsSchema, RootedSupercluster,
+    ArchivePayload, EdgeType, FileWriteBase64OperationInPayload, ParamsSchema, RootedSupercluster,
+    WorkflowStepProcessingResult,
 };
 use petgraph::visit::EdgeRef;
 use std::collections::HashMap;
@@ -10,6 +12,8 @@ use zip::CompressionMethod;
 
 #[host_fn]
 extern "ExtismHost" {
+    fn write_binary_file_base64(payload: FileWriteBase64OperationInPayload) -> ();
+
     // write required host functions here
     // fn get_last_modification_time(relative_path: String) -> SystemTimePayload;
     // will need something to read a file as bytes, probably
@@ -23,15 +27,14 @@ pub fn get_params_schema(_: ()) -> FnResult<ParamsSchema> {
 }
 
 #[plugin_fn]
-pub fn process_paths(_: ArchivePayload) -> FnResult<()> {
-    let zip_path = std::path::Path::new("archive.zip");
-    // may need a host function for this?
-    let zip_file = std::fs::File::create(zip_path).map_err(|e| e.to_string())?;
-    let mut zip = zip::ZipWriter::new(zip_file);
-
-    let supercluster: RootedSupercluster =
-        todo!("Need to supply this, used to just grab the app state.");
-
+pub fn process_paths(archive_payload: ArchivePayload) -> FnResult<WorkflowStepProcessingResult> {
+    // Ok(WorkflowStepProcessingResult {
+    //     hash_set: archive_payload.artifact_mapping,
+    // })
+    // let zip_path = std::path::Path::new("archive.zip");
+    let buffer: std::io::Cursor<Vec<u8>> = std::io::Cursor::new(vec![]);
+    let mut zip = zip::ZipWriter::new(buffer);
+    let supercluster: RootedSupercluster = archive_payload.rooted_supercluster;
     let options = FileOptions::default()
         .compression_method(CompressionMethod::Stored)
         .unix_permissions(0o755);
@@ -91,12 +94,28 @@ pub fn process_paths(_: ArchivePayload) -> FnResult<()> {
         }
 
         let _ = zip.start_file("serialized_complete_graph.yaml", options); // TODO: use result
-        let _ = zip.write(serialized.as_bytes()); // same
+        let _ = zip.write(serialized.as_bytes()); // ditto
     }
-
-    // TODO: add more stuff from before
-
-    zip.finish()
-        .map(|_| zip_path.to_path_buf())
-        .map_err(|ze| ze.to_string())
+    // TODO: add stuff from before to zip
+    let resulting_file = zip.finish();
+    match resulting_file {
+        Ok(cursor) => {
+            let base64_text = BASE64_ENGINE.encode(cursor.into_inner());
+            unsafe {
+                let write_result = write_binary_file_base64(FileWriteBase64OperationInPayload {
+                    relative_path: "archive.zip".to_owned(),
+                    base64_text: base64_text,
+                });
+                write_result
+                    .map(|_| WorkflowStepProcessingResult {
+                        hash_set: archive_payload.artifact_mapping,
+                    })
+                    .map_err(|e| extism_pdk::WithReturnCode(e, 1))
+            }
+        }
+        Err(e) => Err(extism_pdk::WithReturnCode(
+            extism_pdk::Error::msg("Failed to write zip file."),
+            1,
+        )),
+    }
 }
